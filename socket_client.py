e621e527c59a54bee4abfedc4c6a4ccb40e69772
Vadim Shiyanov <93669398+VadimShiyanov@users.noreply.github.com>
2024-01-19 21:06:00 +0300

    Update socket_client.py
    Added comments

--- socket_client.py
+++ socket_client.py
@@ -1,7 +1,19 @@
 import socket
+
+# Create a socket object using IPv4 and TCP
 srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server at the specified address and port
 srv.connect(('localhost', 9339))
+
+# Get input from the user to send as a text message
 t = input("Enter text message \n")
+
+# Encode the text message in UTF-8 and send it to the server
 srv.sendall(t.encode('utf-8'))
+
+# Receive data (up to 1024 bytes) from the server
 data = srv.recv(1024)
+
+# Close the socket connection
 srv.close()
