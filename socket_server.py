9df3b5fb7df61d2eb6d381e480640bc9776e022f
Vadim Shiyanov <93669398+VadimShiyanov@users.noreply.github.com>
2024-01-19 21:07:59 +0300

    Update socket_server.py
    Added comments

--- socket_server.py
+++ socket_server.py
@@ -1,18 +1,41 @@
 import socket
+
+# Create a socket object using IPv4 and TCP
 srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the specified address ('localhost') and port (9339)
 srv.bind(('localhost', 9339))
+
+# Listen for incoming connections with a backlog of 1
 srv.listen(1)
-connection, addres = srv.accept()
+
+# Accept an incoming connection, creating a new socket (connection) for communication
+connection, address = srv.accept()
+
+# Continuous loop for receiving and sending data
 while True:
+    # Receive data (up to 1024 bytes) from the client
     data = connection.recv(1024)
+
+    # Break the loop if no data is received
     if not data:
         break
+
+    # Send the received data back to the client
     connection.sendall(data)
+
+    # Decode the received data as UTF-8
     data = data.decode('utf-8')
-    file = open('text.txt', "w")
-    file.write(data + "\n")
-    file = open('text.txt', "r")
-    for n in file:
-        print(n, end = "")
-    file.close
+
+    # Open a text file ('text.txt') in write mode and write the decoded data
+    with open('text.txt', "w") as file:
+        file.write(data + "\n")
+
+    # Open the text file in read mode and print its content line by line
+    with open('text.txt', "r") as read_file:
+        for n in read_file:
+            print(n, end="")
+
+# Close the file and the connection
+file.close
 connection.close()
